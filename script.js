500a1a107a2fdba40eac3a993f46c5d85229d0ba
Darius <60950039+yourfavouriteowo@users.noreply.github.com>
2022-01-04 23:17:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function openInstagram() {
+  window.open("https://www.instagram.com/chokinghersoftly");
+}
+
+function openDiscord() {
+  window.open("https://discord.com/users/221999158637232128");
+}
+
+function openGitHub() {
+  window.open("https://github.com/yourfavouriteowo");
+}
+
+function sendMail() {
+  window.open("mailto:whoami.420.f@gmail.com")
+}
